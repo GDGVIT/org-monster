2b340fbb6083760fb91ba7b2da47a4909caf9a18
RiddhiGupta5 <mail2riddhigupta@gmail.com>
2020-10-27 22:47:39 +0530

    Added basic view for events of Org

--- org-monster-extension/popup.js
+++ org-monster-extension/popup.js
@@ -1,7 +1,7 @@
 document.getElementById("submit").addEventListener("click", function () {
   var orgName = document.getElementById("orgName").value;
-  document.getElementById("name").innerHTML = orgName;
-  fetch("https://api.github.com/orgs/GDGVIT/events", {
+
+  fetch("https://api.github.com/orgs/" + orgName + "/events", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -10,5 +10,34 @@ document.getElementById("submit").addEventListener("click", function () {
     .then((res) => {
       return res.json();
     })
-    .then((activities) => {});
+    .then((activities) => {
+      var i;
+      var result = "";
+      for (i = 0; i < activities.length; i++) {
+        var activity = activities[i];
+        console.log(activity);
+        var type = activity["type"];
+        console.log(type);
+        console.log(activity["repo"]["name"]);
+        console.log(activity["actor"]["display_login"]);
+        console.log(activity["org"]["login"]);
+        result += type + "<br/>";
+        if (type == "PushEvent") {
+          console.log(activity["payload"]["commits"].length);
+        } else if (type == "PullRequestEvent") {
+          console.log(activity["payload"]["number"]);
+          console.log(activity["payload"]["pull_request"]["title"]);
+        } else if (type == "ForkEvent") {
+          console.log(activity["payload"]["forkee"]);
+        } else if (
+          type == "IssueCommentEvent" ||
+          type == "PullRequestReviewEvent"
+        ) {
+          console.log("Dont show");
+        } else {
+          console.log("UNDEFINED");
+        }
+      }
+      document.getElementById("name").innerHTML = result;
+    });
 });
